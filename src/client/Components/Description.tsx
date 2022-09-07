a10a13005324a6734a6a12a44be4e7eefe0131e7
Eric Moran <emoran0403@gmail.com>
2022-09-07 10:00:50 -0400

    conditional rendering conquered!

--- src/client/Components/Description.tsx
+++ src/client/Components/Description.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const Description = (props: Types.DescriptionCompProps) => {
   const nav = useNavigate();
-  const [descButtonPressed, setDescButtonPressed] = useState<boolean>(true);
+  const [descButtonPressed, setDescButtonPressed] = useState<boolean>(false);
   const [descToHide, setDescToHide] = useState<number>(0);
   const [descButtonHidden, setDescButtonHidden] = useState<boolean>(true);
 
@@ -13,24 +13,26 @@ const Description = (props: Types.DescriptionCompProps) => {
    */
   const showDescriptions = () => {
     console.log(`Showing descriptions`);
-    setDescButtonPressed(false);
+    setDescButtonPressed(true);
+    console.log(descButtonPressed);
   };
 
-  useEffect(() => {}, [descButtonPressed]);
+  useEffect(() => {}, [descButtonPressed, descToHide, descButtonHidden]);
 
   return (
     <div>
       <div>This is Description</div>
       <div>
-        <button onClick={() => showDescriptions()}>Show Description</button>
+        {!descButtonPressed && <button onClick={() => showDescriptions()}>Show Descriptions</button>}
+
         {descButtonPressed && (
           <div>
-            {descToHide === 1 && (
+            {descToHide != 1 && (
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Card Description 1</h5>
                   <p className="card-text">Description 1 text here.</p>
-                  {descButtonHidden && (
+                  {descToHide != 2 && (
                     <button
                       onClick={() => {
                         setDescToHide(1);
@@ -44,13 +46,13 @@ const Description = (props: Types.DescriptionCompProps) => {
               </div>
             )}
 
-            {descToHide === 2 && (
+            {descToHide != 2 && (
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">Card Description 2</h5>
                   <p className="card-text">Description 2 text here.</p>
 
-                  {descButtonHidden && (
+                  {descToHide != 1 && (
                     <button
                       onClick={() => {
                         setDescToHide(2);
@@ -70,7 +72,7 @@ const Description = (props: Types.DescriptionCompProps) => {
             What thoughts do you have about the card and how it relates to your life? What new insights do you have
             after reading the description?
           </p>
-          <input type="text" id="journal2" name="journal2" />
+          <input type="text" id="journal3" name="journal3" />
         </div>
       </div>
     </div>
