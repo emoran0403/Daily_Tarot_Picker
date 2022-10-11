331d5650e8ea2320ab67a693a538cc1ac329371d
Eric Moran <emoran0403@gmail.com>
2022-10-11 10:35:17 -0400

    descriptions are now hidden until user selects a card

--- src/client/DailyDraw/Description.tsx
+++ src/client/DailyDraw/Description.tsx
@@ -5,56 +5,61 @@ const Description = (props: Types.DescriptionCompProps) => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <div className="text-center">
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
+        {!props.cardChosen ? (
+          <div className="accordion" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Description 1
+                </button>
+              </h2>
+              <div
+                id="collapseOne"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample"
               >
-                Description 1
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse"
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                <p>{props.tarotCard.description.one.desc}</p>
+                <div className="accordion-body">
+                  <p>{props.tarotCard.description.one.desc}</p>
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingTwo">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Description 2
+                </button>
+              </h2>
+              <div
+                id="collapseTwo"
+                className="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample"
+              >
+                <div className="accordion-body">
+                  <p>{props.tarotCard.description.two.desc}</p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
-              >
-                Description 2
-              </button>
-            </h2>
-            <div
-              id="collapseTwo"
-              className="accordion-collapse collapse"
-              aria-labelledby="headingTwo"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                <p>{props.tarotCard.description.two.desc}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
+
         <div className="d-flex flex-column justify-content-center align-items-center">
           <h4 className="text-center my-3">
             What thoughts do you have about the card and how it relates to your life? What new insights do you have
--- src/client/DailyDraw/Journal.tsx
+++ src/client/DailyDraw/Journal.tsx
@@ -15,8 +15,8 @@ const Journal = (props: Types.JournalCompProps) => {
 
         <div className="d-flex flex-column justify-content-center align-items-center">
           <h4 className="text-center my-3">
-            Write about how the card pertains to your life in general, a specific situation that you are going through,
-            and what solutions or perspectives it might show you.
+            Before reading the descriptions, write about how the card pertains to your life in general or a specific
+            situation that you are going through, and what solutions or perspectives it might show you.
           </h4>
         </div>
         <textarea className="form-control" wrap="hard" id="journal2" name="journal2" />
