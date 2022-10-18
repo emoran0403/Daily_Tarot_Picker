1e8b97b875bead505a3b242b1ee6864d1f86d566
Eric Moran <emoran0403@gmail.com>
2022-10-18 18:05:19 -0400

    added single diary view for details

---
+++ src/client/Diary/SingleDiary.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import * as Types from "../../../Types";
+import Fetcher from "../ClientUtils/Fetcher";
+import { getNiceDate, getPreview } from "../ClientUtils/Formatters";
+
+// declare an interface for the state passed from diaries view
+interface passedState {
+  diary: Types.IJournalInfo;
+  tarotCard: Types.Card;
+}
+
+const SingleDiary = (props: Types.NO_PROPS) => {
+  const loc = useLocation();
+  const nav = useNavigate();
+
+  const { diary, tarotCard } = loc.state as passedState;
+
+  return (
+    <>
+      {/* {JSON.stringify({ tarotCard, diary })} */}
+      <div className="container">
+        <div className="card mt-4">
+          <h6 className="text-center mt-2 display-6">
+            {getNiceDate(diary.created_at!)} | {tarotCard.name}
+            <span className="ms-4">
+              <button className="btn btn-primary" onClick={() => nav(-1)}>
+                Back
+              </button>
+            </span>
+          </h6>
+
+          <div className="row">
+            <div className="col-6">
+              <div className="shadow-lg">
+                <img src={`${tarotCard.url}`} className="card-img-top" alt={`${tarotCard.name}`} />
+              </div>
+            </div>
+            <div className="col-6 ">
+              <div className="shadow-lg">
+                <div className="card-body h-100">
+                  <h5 className="card-title">Entry 1</h5>
+                  <p>{diary.entry_one}</p>
+                  <h5 className="card-title">Entry 2</h5>
+                  <p>{diary.entry_two}</p>
+                  <h5 className="card-title">Entry 3</h5>
+                  <p>{diary.entry_three}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleDiary;
