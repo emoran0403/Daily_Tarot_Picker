b82c02f148665647012f552c5fa1660f3e98829c
Eric Moran <emoran0403@gmail.com>
2022-10-18 18:04:49 -0400

    abstracted formatting functions to their own file and folder

---
+++ src/client/ClientUtils/Formatters.ts
@@ -0,0 +1,18 @@
+// prettifies the date from the database
+export const getNiceDate = (date: string) => {
+  // "MonthAsText, day, year"
+  const dateObject = new Date(date);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  // @ts-ignore
+  return dateObject.toLocaleDateString("en-US", options);
+};
+
+// shortens diary entries and conditionally appends ellipses
+export const getPreview = (string: string) => {
+  const length = string.length;
+  if (length >= 100) {
+    return string.substring(0, 100) + "...";
+  } else {
+    return string;
+  }
+};
